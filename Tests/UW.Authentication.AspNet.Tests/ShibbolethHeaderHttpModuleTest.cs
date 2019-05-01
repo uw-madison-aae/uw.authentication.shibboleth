3c9b4e4fa7fdcda40ae52cfc35e4ad29f67d7293
Dieckman, Eric <eric.dieckman@wisc.edu>
2019-05-01 08:16:35 -0500

    remove UW.Identity

--- Tests/UW.Authentication.AspNet.Tests/ShibbolethHeaderHttpModuleTest.cs
+++ Tests/UW.Authentication.AspNet.Tests/ShibbolethHeaderHttpModuleTest.cs
@@ -51,5 +51,83 @@ namespace UW.Authentication.AspNet.Tests
 
 
         }
+
+        [Fact]
+        public void GetAttributesFromRequest_5AttributesNoExtras_Returns5Attributes()
+        {
+            using (var simulator = new HttpSimulator())
+            {
+
+                simulator.SetHeader("uid", "bbadger");
+                simulator.SetHeader("mail", "bucky.badger@wisc.edu");
+                simulator.SetHeader("givenName", "Bucky");
+                simulator.SetHeader("sn", "Badger");
+                simulator.SetHeader("wiscEduPVI", "UW999B999");
+
+                simulator.SimulateRequest();
+
+                var module = new ShibbolethHeaderHttpModule();
+
+                var expected = 5;
+
+                var actual = module.GetAttributesFromRequest(HttpContext.Current.Request).Count;
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void GetAttributesFromRequest_5AttributesWithExtras_Returns5Attributes()
+        {
+            using (var simulator = new HttpSimulator())
+            {
+
+                simulator.SetHeader("uid", "bbadger");
+                simulator.SetHeader("mail", "bucky.badger@wisc.edu");
+                simulator.SetHeader("givenName", "Bucky");
+                simulator.SetHeader("sn", "Badger");
+                simulator.SetHeader("wiscEduPVI", "UW999B999");
+                simulator.SetHeader("SERVER_NAME", "rocking.wisc.edu");
+                simulator.SetHeader("REQUEST_METHOD", "GET");
+
+                simulator.SimulateRequest();
+
+                var module = new ShibbolethHeaderHttpModule();
+
+                var expected = 5;
+
+                var actual = module.GetAttributesFromRequest(HttpContext.Current.Request).Count;
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public void GetAttributesFromRequest_CommonAttributes_EqualsHeaderValues()
+        {
+            using (var simulator = new HttpSimulator())
+            {
+
+                simulator.SetHeader("uid", "bbadger");
+                simulator.SetHeader("mail", "bucky.badger@wisc.edu");
+                simulator.SetHeader("givenName", "Bucky");
+                simulator.SetHeader("sn", "Badger");
+                simulator.SetHeader("wiscEduPVI", "UW999B999");
+
+                simulator.SimulateRequest();
+
+                var module = new ShibbolethHeaderHttpModule();
+
+
+                var attribs = module.GetAttributesFromRequest(HttpContext.Current.Request);
+
+                Assert.Equal(HttpContext.Current.Request.Headers["uid"], attribs["uid"].Value);
+                Assert.Equal(HttpContext.Current.Request.Headers["mail"], attribs["mail"].Value);
+                Assert.Equal(HttpContext.Current.Request.Headers["givenName"], attribs["givenName"].Value);
+                Assert.Equal(HttpContext.Current.Request.Headers["sn"], attribs["sn"].Value);
+                Assert.Equal(HttpContext.Current.Request.Headers["wiscEduPVI"], attribs["wiscEduPVI"].Value);
+            }
+        }
+
     }
 }
