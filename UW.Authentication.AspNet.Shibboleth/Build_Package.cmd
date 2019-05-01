18e5045e4759c7341083462bc3377e4d7dfd6445
Dieckman, Eric <eric.dieckman@wisc.edu>
2019-05-01 09:21:00 -0500

    convert aspnet library to new format

--- UW.Authentication.AspNet.Shibboleth/Build_Package.cmd
+++
@@ -1,3 +0,0 @@
-@if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin" set PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin;%PATH%
-@if exist "%ProgramFiles(x86)%\NuGet" set PATH=%ProgramFiles(x86)%\NuGet;%PATH%
-msbuild UW.Authentication.AspNet.csproj /t:Build;Package;Publish /p:Configuration="Release"
